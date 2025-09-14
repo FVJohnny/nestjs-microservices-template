f215cd4f1d449c111865f3dbc7b0f27a411f58cd
FVJohnny <jofervac@gmail.com>
2025-09-14 16:54:25 +0200

    chore: remove unnecessary comments from export statements

--- backend/libs/common/src/general/index.ts
+++ backend/libs/common/src/general/index.ts
@@ -1,11 +1,7 @@
-// === DOMAIN ===
 export * from './domain';
 
-// === APPLICATION ===
 export * from './application';
 
-// === INFRASTRUCTURE ===
 export * from './infrastructure';
 
-// === INTERFACES ===
 export * from './interfaces';
--- backend/libs/common/src/index.ts
+++ backend/libs/common/src/index.ts
@@ -1,37 +1,13 @@
-// Heartbeat
 export * from './heartbeat';
-
-// Swagger utilities
 export * from './swagger';
-
-// Error handling exports
 export * from './errors';
-
-// Auth exports
 export * from './auth';
-
-// Metrics exports
 export * from './metrics';
-
-// Testing utilities
 export * from './testing';
-
-// General exports
 export * from './general';
-
-// Integration events
 export * from './integration-events';
-
-// Event tracker (integration + domain)
 export * from './event-tracker';
-
-// Outbox pattern
 export * from './outbox';
-
-// Utility exports
 export * from './utils';
-
-// Tracing
 export * from './tracing';
-// Logger
 export * from './logger';
