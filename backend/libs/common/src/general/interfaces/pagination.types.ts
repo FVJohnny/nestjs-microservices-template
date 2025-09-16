aa43306fb184ceaf1042abaf4f1cee3006890104
FVJohnny <jofervac@gmail.com>
2025-09-16 12:35:20 +0200

    refactor: remove unused Transform import from pagination types

--- backend/libs/common/src/general/interfaces/pagination.types.ts
+++ backend/libs/common/src/general/interfaces/pagination.types.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional, IsString, IsNumber } from 'class-validator';
-import { Type, Transform } from 'class-transformer';
+import { Type } from 'class-transformer';
 
 import { OrderTypes } from '../domain/criteria/order/OrderType';
 
